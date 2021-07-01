27af0b0c92bb062695656e01bef3f646b0f052f3
Jae Lee <jael@stanford.edu>
2021-07-01 12:32:08 -0700

    Add debug

--- src/ReviewerReport.php
+++ src/ReviewerReport.php
@@ -22,6 +22,7 @@ if (!$record) {
 
 $get_fields = array('applicant_name', 'reviewer_summary', 'program_v2');
 $q = REDCap::getData('array',array($record), $get_fields);
+$module->emDebug($q);
 
 $record_result = $q[$record];
 //$review_events = array(67556, 67557, 67558, 67559, 67560);
@@ -47,7 +48,7 @@ if ($round_2 == "2") {
 $str = '';
 foreach ($review_events as $event_id) {
     $i++;
-    //    Plugin::log($event_id, "DEBUG", "event id at $i");
+    $module->emDebug("$event_id event id at $i");
 
     $review = $record_result[$event_id]['reviewer_summary'];
     //143 seems to be the char length of an empty summarize field
